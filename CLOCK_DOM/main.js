4dd9d40a517c91ed6418802e469e8a6e2576b0e3
Vlad Kazachenko <kazachenko2v@gmail.com>
2020-02-26 17:41:29 +0300

    second commit

--- CLOCK_DOM/main.js
+++ CLOCK_DOM/main.js
@@ -8,7 +8,11 @@ function showTime(){
     let hours = currentTime.getHours();
     let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
-    let qwe = (q) => q<10 ?'0' + q: q;
+    
+    let qwe = function(q){
+        if (q<10) { q = '0' + q }
+        return q;
+    }
 
     time.innerHTML = qwe(hours) + ':' + qwe(minutes) + ':' + qwe(seconds);
     let degSec = 6 * seconds;
