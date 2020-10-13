1d873424bfbb320a8ae8193b1c7beb565c193ea2
Vlad Kazachenko <kazachenko2v@gmail.com>
2020-10-13 15:58:30 +0300

    commit 2

--- test/js.js
+++ test/js.js
@@ -1,14 +1,29 @@
-    let view = document.getElementById('view');
-
-    view.onclick = function(){
-      let textarea = document.createElement('textarea');
-      textarea.setAttribute('class', this.getAttribute('class'));
-      textarea.value = this.innerHTML;
-      this.replaceWith(textarea);
-      textarea.focus();
-      
-      textarea.onblur = function(){
-        textarea.replaceWith(view);
-        view.innerHTML = textarea.value;
-      }
+mouse.onfocus = function(event){
+  alert('we')
+  let target = event.target;
+  target.style.position = 'absolute';
+  document.onkeydown = function(event){
+    let mouseWidth = mouse.offsetWidth;
+    let mouseHeight = mouse.offsetHeight;
+    let newPosLeft = mouse.getBoundingClientRect().left;
+    let newPosTop = mouse.getBoundingClientRect().top;
+    switch(event.code) {
+      case 'ArrowRight':
+        newPosLeft += mouseWidth;
+        break;
+      case 'ArrowDown':
+        newPosTop +=  mouseHeight;
+        break;
+      case 'ArrowLeft':
+        newPosLeft = newPosLeft - mouseWidth;
+        break;
+      case 'ArrowTop':
+        newPosTop = newPosTop - mouseHeight;
+        break;
     }
+
+    mouse.style.left = newPosLeft + 'px';
+    mouse.style.top = newPosTop + 'px';
+
+  }
+}
